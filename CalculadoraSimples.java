8dc7de2276ddf01506c395bd748d937d36545566
muhcrz <cruzmurilohenrique@gmail.com>
2025-04-17 08:53:55 -0300

    commit murilo

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -1,39 +1,55 @@
 import java.util.Scanner;
 
-public class CalculadoraSimples{
-    public static void main(String[] args){
-        //Declaração de variáveis
-        int n1, n2, total;
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int n1, n2, total, op;
 
-        //o otavio esteve aqui
-
-        //Criação e instância do objeto de entrada
+        // Criação e instância do objeto de entrada
         Scanner entrada = new Scanner(System.in);
-        
-        //Apresentação
+
+        // Apresentação
         System.out.println("\n\t\t\t -- Calculadora Simples --\n");
 
-        //Entradas
-        System.out.println("Soma Simples: ");
-        System.out.print("Informe N1: ");
-        n1 = entrada.nextInt();
-        System.out.print("Informe N2: ");
-        n2 = entrada.nextInt();
+        // Menu
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Sair");
 
-        //Processamento
-        total = n1 + n2;
+        // Opção do usuário
+        System.out.print("Opção: ");
+        op = entrada.nextInt();
 
-        //Saída
-        System.out.println(n1 + " + " + n2 + " = " + total);
+        if (op == 1) {
+            // Entrada
+            System.out.println("\nSoma Simples:");
+            System.out.print("Informe N1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe N2: ");
+            n2 = entrada.nextInt();
 
-         System.out.print("1 - Soma ");
-         System.out.println("2 - Subtração");
-         System.out.println("3 - Sair);");
+            total = n1 + n2;
+            System.out.println(n1 + " + " + n2 + " = " + total);
 
-         //Menu
-         System.out.println("1 - soma ");
-         System.out.println("2 -Subtração ");
-         System.out.println("3 - Sair");
+        } else if (op == 2) {
+            // Entrada
+            System.out.println("\nSubtração:");
+            System.out.print("Informe N1: ");
+            n1 = entrada.nextInt();
+            System.out.print("Informe N2: ");
+            n2 = entrada.nextInt();
 
+            total = n1 - n2;
+            System.out.println(n1 + " - " + n2 + " = " + total);
+
+        } else if (op == 3) {
+            // Sair
+            System.out.println("\nForte abraço!");
+        } else {
+            // Opção inválida
+            System.out.println("\nOpção " + op + " incorreta!");
+        }
+
+       
     }
 }
